97a184340e96a0660e2a2d6277a819c300a0be1a
dilini1995s <dilinitharanga721@gmail.com>
2020-07-10 07:23:32 +0530

    user table add

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+$router->post('/login', 'LoginController@login');
+$router->post('/register', 'UserController@register');
+$router->get('/user', ['middleware' => 'auth', 'uses' =>  'UserController@get_user']);
